e143b661c8357cd4f40ee28afa6fcab955fc360e
Sebas <sebas@LinuxMint.Sebas>
2022-12-02 21:10:31 -0300

    inicio del crud php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'crud-php'
+
+);
+
+/*
+if (isset($conn)) {
+    echo 'Base de Datos Conectada';
+}
+
+*/
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?include("db.php");?>
+
+
+
+html
